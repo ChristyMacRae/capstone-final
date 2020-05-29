3d69af3fce6fb23016e509a143f66e5282a0ba80
Christy MacRae <christymacrae@googlemail.com>
2020-05-29 17:52:40 +0100

    Same again....migrations

--- models.py
+++ models.py
@@ -4,8 +4,8 @@ from datetime import date
 import json
 from flask_migrate import Migrate
 
-# database_path = os.environ['DATABASE_URL']
-database_path = 'postgres://postgres@localhost:5432/capstone'
+database_path = os.environ['DATABASE_URL']
+# database_path = 'postgres://postgres@localhost:5432/capstone'
 # database_path = 'postgres://txoytuiksvhssc:a7d9ca8df13af8faec463783b2c0cc3506b6ff348f6d2afec54af9a212abc09b@ec2-35-174-88-65.compute-1.amazonaws.com:5432/d4a91cto59cs85'
 
 db = SQLAlchemy()
